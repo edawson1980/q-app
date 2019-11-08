707ec78a8b7e8f149e0890ea628bb6fdc1bb027e
Erin <erinedawson@gmail.com>
2019-11-08 15:09:08 -0700

    add logic to Form component

--- src/Form.js
+++ src/Form.js
@@ -4,11 +4,35 @@ import Tip from './Tip.js';
 import Total from './Total.js';
 
 class Form extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {value: ''};
+
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  };
+
+
+
+  handleSubmit(event){
+    let x = this.state.value;
+    if(x != NaN){
+      alert('Thanks!');
+    }else{
+      alert('Please input a numeric value')
+    }
+    event.preventDefault();
+  }
+
   render(){
     return(
       <div className="app-body">
-        <form>
-          <input type="text" name="total" placeholder="Total After Tax"></input>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="total" value={this.state.value} onChange={this.handleChange} placeholder="Total After Tax"></input>
           <select name="tip">
             <option value=".1">10%</option>
             <option value=".15">15%</option>
@@ -16,7 +40,7 @@ class Form extends React.Component{
             <option value=".2">20%</option>
             <option value=".22">22%</option>
           </select>
-          <input type="text" name="party" placeholder="Size of Party"></input>
+          <input type="text" value={this.state.value} onChange={this.handleChange} name="party" placeholder="Size of Party"></input>
           <input type="submit" value="Calculate Tip"/>
         </form>
         <Tip />
