5181360893a8c3adf05bded3a81d68d6e25b6038
Erin <erinedawson@gmail.com>
2019-11-08 09:56:22 -0700

    add form fields and dropdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,6 +9,20 @@ function App() {
           Tip Calculator
         </h1>
       </header>
+      <div className="app-body">
+        <form>
+          <input type="number" name="total" placeholder="Total After Tax"></input>
+          <select name="tip">
+            <option value='.1'>10%</option>
+            <option value='.15'>15%</option>
+            <option value='.18'>18%</option>
+            <option value='.2'>20%</option>
+            <option value='.22'>22%</option>
+          </select>
+          <input type="number" name="party" placeholder="Size of Party"></input>
+        </form>
+
+      </div>
     </div>
   );
 }
