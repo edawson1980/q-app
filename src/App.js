60ecd5d5dd8ce21d2dd2072168f81f47af337454
Erin <erinedawson@gmail.com>
2019-11-08 10:25:13 -0700

    add submit button

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ function App() {
         <form>
           <input type="text" name="total" placeholder="Total After Tax"></input>
           <select name="tip">
-            <option value='.1'>10%</option>
-            <option value='.15'>15%</option>
-            <option value='.18'>18%</option>
-            <option value='.2'>20%</option>
-            <option value='.22'>22%</option>
+            <option value=".1">10%</option>
+            <option value=".15">15%</option>
+            <option value=".18">18%</option>
+            <option value=".2">20%</option>
+            <option value=".22">22%</option>
           </select>
           <input type="text" name="party" placeholder="Size of Party"></input>
+          <input type="submit" value="Calculate Tip"/>
         </form>
 
       </div>
